ae1fdc97598d6b7d2397dbe109ffd974cc1d4fad
Machine learning & Python <eduardo@webean.org>
2015-10-08 20:29:50 -0700

    Update naive-bayes-continuo-teorema-de-bayes-python.py

--- naive-bayes-continuo-teorema-de-bayes-python.py
+++ naive-bayes-continuo-teorema-de-bayes-python.py
@@ -1,3 +1,10 @@
 import csv
 import random
 import math
+
+def loadCsv(archivo):
+	lines = csv.reader(open(archivo, "rb"))
+	datos = list(lines)
+	for i in range(len(datos)):
+		datos[i] = [float(x) for x in datos[i]]
+	return datos
